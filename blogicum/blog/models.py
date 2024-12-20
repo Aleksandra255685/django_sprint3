13ca81d6b27d00fd95c758e8f811fd854d204c05
Aleksandra255685 <sashaven2003@mail.ru>
2024-12-21 00:55:41 +0700

    fix errors

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -23,7 +23,7 @@ class Category(CreatedPublished):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text='''Идентификатор страницы для URL; 
+        help_text='''Идентификатор страницы для URL;
         разрешены символы латиницы, цифры, дефис и подчёркивание.'''
     )
 
@@ -51,15 +51,14 @@ class Post(CreatedPublished):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text='''Если установить дату и время в будущем — 
+        help_text='''Если установить дату и время в будущем —
         можно делать отложенные публикации.'''
     )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         verbose_name='Автор публикации',
-        related_name='posts'
-    )
+        related_name='posts')
     location = models.ForeignKey(
         Location,
         on_delete=models.SET_NULL,
